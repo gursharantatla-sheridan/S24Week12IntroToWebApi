720187ba9b1f605f1f01e145ab569d8c44755f08
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-31 08:41:10 -0400

    Add project files.

---
+++ S24Week12IntroToWebApi/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace S24Week12IntroToWebApi
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string? Title { get; set; }
+        public double? Price { get; set; }
+    }
+}
---
+++ S24Week12IntroToWebApi/BookContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace S24Week12IntroToWebApi
+{
+    public class BookContext : DbContext
+    {
+        // entity set
+        public DbSet<Book> Books { get; set; }
+
+        // constructor
+        public BookContext(DbContextOptions<BookContext> options) : base(options) 
+        { }
+
+        // data seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "HTML", Price = 10 },
+                new Book { BookId = 2, Title = "ASP.NET", Price = 20 }
+            );
+        }
+    }
+}
