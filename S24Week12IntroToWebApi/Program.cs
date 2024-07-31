0d7ac63a55dc3e4e8e2c2e89c3c7724e00cc636b
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-31 08:46:58 -0400

    conn str

--- S24Week12IntroToWebApi/Program.cs
+++ S24Week12IntroToWebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace S24Week12IntroToWebApi
 {
     public class Program
@@ -13,6 +15,9 @@ namespace S24Week12IntroToWebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // define the connection string
+            builder.Services.AddDbContext<BookContext>(opt => opt.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BooksWebApi;Integrated Security=true;MultipleActiveResultSets=true"));
 
             var app = builder.Build();
 
